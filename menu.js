0522e12eb31c6faf37561aa37bb9d3577af8705a
haridulal1000 <84311145+haridulal1000@users.noreply.github.com>
2022-01-04 22:23:39 +0545

    first-commit

---
+++ menu.js
@@ -0,0 +1,36 @@
+const submenuList = document.querySelectorAll('.sub-folder-item');
+submenuList.forEach(function(e) {
+    e.style.display = 'none';
+});
+document.querySelector('#properties-menu').style.display = 'block';
+const menus = document.querySelectorAll('.folder-item');
+for (let i = 0; i < menus.length; i++) {
+    menus[i].addEventListener('click', function(e) {
+        let id = e.target.getAttribute('id');
+        console.log(id);
+        let menus = document.querySelectorAll('.folder-item');
+        for (let j = 0; j < menus.length; j++) {
+            menus[j].classList.remove('active');
+        }
+        if (!e.target.classList.contains('active')) {
+            e.target.classList.add('active');
+        }
+        let element = document.querySelector(`#${id}-menu`);
+        console.log(element);
+        console.log(`#${id}-menu`);
+        if (element) {
+            let submenus = document.querySelectorAll(`.${element.classList[0]}`);
+            console.log(submenus);
+            for (let j = 0; j < submenus.length; j++) {
+                submenus[j].style.display = 'none';
+            }
+            element.style.display = 'block';
+        } else {
+            let submenus = document.querySelectorAll(`.sub-folder-item`);
+            for (let j = 0; j < submenus.length; j++) {
+                submenus[j].style.display = 'none';
+            }
+            document.querySelector('#properties-menu').style.display = 'block';
+        }
+    });
+}
