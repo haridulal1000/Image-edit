2b0557d5fd45398271e0ee87c9744ec7e3a2e41e
haridulal1000 <84311145+haridulal1000@users.noreply.github.com>
2022-01-07 16:17:32 +0545

    fixed move tool

--- js/move.js
+++ js/move.js
@@ -1,11 +1,28 @@
 let currentTool;
+let move = false;
 document.getElementById('viewport').addEventListener('mousedown', function(e) {
-    if (currentTool === 'move') {
+    if (currentTool === 'move' && move === false) {
+        move = true;
+    }
+});
+document.getElementById('viewport').addEventListener('mouseup', function(e) {
+    if (currentTool === 'move' && move === true) {
+        move = false;
+    }
+});
+document.getElementById('viewport').addEventListener('mousemove', function(e) {
+    if (currentTool === 'move' && move === true) {
         e.preventDefault();
+
+        let rect = document.getElementById('viewport').getBoundingClientRect();
+        // var x = e.clientX - rect.left; //x position within the element.
+        // var y = e.clientY - rect.top; //y position within the element.
+
+
         for (let i = 0; i < layers.length; i++) {
             if (selectedLayer == layers[i].id) {
-                layers[i].x = e.offsetX;
-                layers[i].y = e.offsetY;
+                layers[i].x = e.clientX - rect.left - layers[i].width / 2;
+                layers[i].y = e.clientY - rect.top - layers[i].height / 2;
                 renderLayersAll();
                 break;
             }
